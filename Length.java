819fbe214f7c5f522f26743f6ec80edf6c2fa25b
barbadeadesh <barbadeadesh2020@email.com>
2022-01-31 22:28:26 +0530

    calculate the length

---
+++ Length.java
@@ -0,0 +1,24 @@
+package linecomparison;
+import java.util.Scanner;
+
+
+
+public class uc1_Length{
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+        double x1,x2,y1,y2,distance;
+        
+        System.out.println("enter x1 & y1 values of first point");
+        x1=sc.nextDouble();
+        y1=sc.nextDouble();
+        
+        System.out.println("enter x2 & y2 values of second point");
+        x2=sc.nextDouble();
+        y2=sc.nextDouble();
+        
+        distance=(int)Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Distance between two points = "+distance);
+	}
+
+}
